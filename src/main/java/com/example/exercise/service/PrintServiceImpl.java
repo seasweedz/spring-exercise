ac1adeb80cac6a38a879ee8fcffc1aa32c7f8488
weerapat.boonthanan <imixzba@gmail.com>
2021-03-05 12:45:45 +0700

    Add result to text file function

--- src/main/java/com/example/exercise/service/PrintServiceImpl.java
+++ src/main/java/com/example/exercise/service/PrintServiceImpl.java
@@ -3,10 +3,16 @@ package com.example.exercise.service;
 import com.example.exercise.model.KeyValue;
 import com.example.exercise.model.PrintResponse;
 import com.example.exercise.model.Response;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,7 +31,25 @@ public class PrintServiceImpl  implements   PrintService{
 
         resp.setFormattedResult(formattedList);
 
-        return new ResponseEntity<>(new Response<>(resp), HttpStatus.OK);
+        var result = new Response<>(resp);
+        WriteResult(result);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    private void WriteResult(Response<PrintResponse> result){
+        ObjectMapper mapper = new ObjectMapper();
+        try{
+            String jsonStr = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(result);
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH-mm-ss");
+            LocalDateTime now = LocalDateTime.now();
+            FileOutputStream outputStream = new FileOutputStream(dtf.format(now)+".txt");
+            byte[] strBytes = jsonStr.getBytes(StandardCharsets.UTF_8);
+            outputStream.write(strBytes);
+
+            outputStream.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
 
